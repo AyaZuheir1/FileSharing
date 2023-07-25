147edb7624a1038beb35502d21de11c396320b58
ayazuheir2@gmail.com <ayazuheir2@gmail.com>
2023-07-25 20:10:34 +0300

    filesSharing

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -9,7 +9,7 @@
     @foreach ($files as $file)
         <div>
             <h3>{{ $file->file_name }}</h3>
-            <img src="{{ Storage::url($file->file_path) }}" alt="{{ $file->file_name }}" style="max-width: 400px; max-height: 400px;">
+            <img src="{{ route('file.download', $file->id) }}" alt="{{ $file->file_name }}" style="max-width: 400px; max-height: 400px;">
         </div>
     @endforeach
 </body>
